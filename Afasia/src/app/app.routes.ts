d8f3b48b28ada68feb8fd187d36a3b3ae9ca0a5a
Alvaro Martino <alva.98.am@gmail.com>
2020-10-08 14:38:17 -0300

    Agrego a la navegacion la ventana de contacto y le agrego animaciones de fadeIn

--- Afasia/src/app/app.routes.ts
+++ Afasia/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { RouterModule, Routes } from '@angular/router'
 import { MenuComponent } from './components/menu/menu.component';
 import { RegistroComponent } from "./components/registro/registro.component";
+import { ContactoComponent } from "./components/bodycontacto/body.component";
 
 const APP_ROUTES: Routes =[
     { path: 'menu', component: MenuComponent},
     { path: 'registro', component: RegistroComponent },
+    { path: 'contacto', component: ContactoComponent },    
     { path: '**', pathMatch: 'full', redirectTo:'menu'} 
 
 ];
